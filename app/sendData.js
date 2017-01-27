81acb0956a334906cfc65621750830e6f21b1993
johorst <mart1601@web.de>
2017-01-27 10:47:34 +0000

    sdfsfo

--- app/sendData.js
+++ app/sendData.js
@@ -1,13 +1,11 @@
-function postBet(bet) {
-var createorsaveUrl = "https://blap-johorst.c9users.io/setzen";
+function postBet(bet, createorsaveUrl) {
 var y = new XMLHttpRequest();
 y.open('POST', createorsaveUrl);
 y.responseType = 'json';
 y.contentType = 'application/json';
 y.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
 y.onload = function(){window.mystatus = "Aktion POST " + this.name + " erfolgreich";
-console.log(y.response)};;
+console.log(y.response)};
 y.error = function(){window.mystatus = "Aktion POST NICHT erfolgreich"};
 y.send(JSON.stringify(bet));
-//console.log(bet);
 }
