352973e29f259e18d33086aa04bd766905fbe4aa
johorst <mart1601@web.de>
2017-01-27 10:46:42 +0000

    asd

--- app/ape.js
+++ app/ape.js
@@ -1,28 +1,11 @@
-var t = [
-		 {
-		  team1 : "Stuttgart",
-		  team2 : "Kickers",
-		  quote1 : 1.03,
-		  quote0 : 0.03,
-		  quote2 : 2.03},
-		 {
-		  team1 : "Sonnenhof",
-		  team2 : "Fürth",
-		  quote1 : 10.03,
-		  quote0 : 0.03,
-		  quote2 : 20.03},
-		 {
-		  team1 : "Backnang",
-		  team2 : "VfB",
-		  quote1 : 11.03,
-		  quote0 : 0.03,
-		  quote2 : 22.03}
-		 ];
-
- // Creating the module called blwett
 var blwett = angular.module("blwett", []);
- // Registering the blwettCtrl to the blwett module
-blwett.controller("blwettCtrl", function ($scope) {
-	 // Binding the car's array to the scope
-	 $scope.begegnungen = t;
- });
+blwett.controller("blwettCtrl", function ($scope, $http) {
+    $http.get("./databas/18wsq.json").success(function( data ) {
+    $scope.begegnungen = data;
+    });
+    $http.get("./kto.json").success(function( data ) {
+    $scope.user = data.kto.user;
+    $scope.saldo = data.kto.saldo;
+    window.glvaruser =  data.kto.user;
+    });
+});
